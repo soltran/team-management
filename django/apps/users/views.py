a8efe68ac7f00f2ceb0f7a3b27a311d180b62f4e
Sol Tran <hi@soltran.io>
2024-10-03 11:28:44 -0700

    set restrictions on actions users can take based on role

--- django/apps/users/views.py
+++ django/apps/users/views.py
@@ -4,18 +4,47 @@ from .serializers import CustomUserSerializer, CompanySerializer
 
 class IsSuperuserOrCompanyAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_superuser or request.user.is_company_admin  # Remove the parentheses
+        return request.user.is_superuser or request.user.is_company_admin
+
+class CanManageCompanyUsers(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Allow superusers full access
+        if request.user.is_superuser:
+            return True
+        
+        # Company admins can create new users
+        if request.user.is_company_admin and request.method == 'POST':
+            return True
+        
+        # For other methods, defer to has_object_permission
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        # Allow superusers full access
+        if request.user.is_superuser:
+            return True
+        
+        # Company admins can manage users within their company
+        if request.user.is_company_admin and obj.company == request.user.company:
+            # For DELETE and PATCH methods (delete user or change role)
+            if request.method in ['DELETE', 'PATCH']:
+                return True
+            # For other methods, allow read access
+            return request.method in permissions.SAFE_METHODS
+        
+        # Regular users can only read their own data
+        return request.method in permissions.SAFE_METHODS
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated, IsSuperuserOrCompanyAdmin]
+    permission_classes = [permissions.IsAuthenticated, CanManageCompanyUsers]
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_staff:
+        if user.is_superuser:
             return CustomUser.objects.all()
-        else:
+        else
             return CustomUser.objects.filter(company=user.company)
 
 class CompanyViewSet(viewsets.ModelViewSet):
