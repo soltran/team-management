6fb601d148507bc4583b56f733cb1862ac097b36
Sol Tran <hi@soltran.io>
2024-10-03 00:16:29 -0700

    make sure user needs to be authenticated to access team members

--- django/team/views.py
+++ django/team/views.py
@@ -2,20 +2,9 @@ from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .models import TeamMember
-from .serializers import TeamMemberSerializer, UserSerializer
+from .serializers import TeamMemberSerializer
 
 class TeamMemberViewSet(viewsets.ModelViewSet):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (permissions.AllowAny,)
-    serializer_class = UserSerializer
-
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    serializer_class = UserSerializer
-
-    def get_object(self):
-        return self.request.user
+    permission_classes = [permissions.IsAuthenticated]
